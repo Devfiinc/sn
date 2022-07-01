b619d9ee443299aa1db6ac0ad1a79132063a8dd2
icirauqui <icirauqui@gmail.com>
2022-07-01 17:07:06 +0200

    nn pipeline

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,8 @@ pub mod nn;
 type VecVec64 = Vec<Vec<Option<f64>>>;
 
 
+use rand::thread_rng;
+use rand::seq::SliceRandom;
 
 
 
@@ -50,8 +52,9 @@ fn main() -> Result<(), Error> {
                      row.get(20)]);
     }
     
-    //println!("{}", sn[0][0].unwrap());
-
+    
+    // Randomize input
+    sn.shuffle(&mut thread_rng());
 
 
     // Split dataset into train and test
@@ -65,20 +68,41 @@ fn main() -> Result<(), Error> {
     for n in sn {
         if i < split {
             x_train_1.push(n[0..20].to_vec());
-            y_train_1.push(n[20]);
+            y_train_1.push(n[20].clone());
         } else {
             x_test_1.push(n[0..20].to_vec());
-            y_test_1.push(n[20]);
+            y_test_1.push(n[20].clone());
         }
         i = i + 1;
     }
 
+    for i in 0..100 {
+        println!("{} {} {}", x_train_1[i][0].unwrap(), x_train_1[i][1].unwrap(), y_train_1[i].unwrap());
 
+    }
 
+/*
+    let mut cnt : Vec<i64> = vec![0; 10];
+
+    for n in y_test_1 {
+        let a = n.unwrap() as i64 as usize;
+        cnt[a] = cnt[a] + 1;
+    }
+
+    for i in 0..10 {
+        println!("{}", cnt[i]);
+    }
+    */
+
+    /*
+    for i in 0..y_train_1.len() {
+        cnt[y_train_1[i].unwrap() as usize] = cnt[y_train_1[i].unwrap() as usize] + 1;
+    }
+    */
 
 
     
-
+/*
 
     let mut x_train = Array::from_elem((x_train_1.len(), 20), 0.);
     let mut y_train = Array::from_elem((y_train_1.len(), 1), 0.);
@@ -113,15 +137,14 @@ fn main() -> Result<(), Error> {
     }
 
 
+*/
 
 
 
 
+    
 
-
-
-
-    let size_li = x_train.dim().1;
+    let size_li = x_train_1[0].len();
     let size_l1 = 30;
     let size_l2 = 30;
     let size_lo = 10;
@@ -158,23 +181,29 @@ fn main() -> Result<(), Error> {
     let mut idx : usize = 0;
     for x in x_train_1 {
         li = na::DMatrix::<f64>::from_vec(size_li, 1, x.iter().map(|x| x.unwrap()).collect());
+        println!("li {}", li[(0,0)]);
+        println!("wi1 {}", wi1[(0,0)]);
 
-        l1 = li.transpose() * &wi1;
+        l1 = &li.transpose() * &wi1;
         a1 = l1.map(|x| nn::NN::sigmoid(x));
+
+        println!("l1 {}", l1[(0,0)]);
+        println!("a1 {}", a1[(0,0)]);
 
         l2 = l1 * &w12;
         a2 = l2.map(|x| nn::NN::sigmoid(x));
 
         lo = l2 * &w2o;
-        ao = lo.map(|x| nn::NN::sigmoid(x));
+        ao = lo.map(|x| nn::NN::softmax(x));
         
         let y : i64 = y_train_1[idx].unwrap() as i64;
         let mut ly = na::DMatrix::from_element(size_lo, 1, 0.);
         ly[(y as usize,0)] = 1.;
 
-        deltao = lo - ly.transpose();
+
+        deltao = ao - ly.transpose();
         delta2 = &w2o * deltao.transpose();
-        delta2 = delta2.component_mul(&a2.transpose().map(|x| nn::NN::sigmoid_derivative(x)));
+        delta2 = delta2.component_mul(&a2.transpose().map(|x| nn::NN::softmax_derivative(x)));
 
         delta1 = &w12.transpose() * &delta2;
         delta1 = delta1.component_mul(&a1.transpose().map(|x| nn::NN::sigmoid_derivative(x)));
@@ -183,11 +212,25 @@ fn main() -> Result<(), Error> {
         w12 = w12.clone() - &(delta2 * &a1 * learning_rate).transpose();
         wi1 = wi1.clone() - &(delta1 * &li.transpose() * learning_rate).transpose();
 
-        println!("{}", idx);
+        println!("{} {}", idx, y);
         idx += 1;
+
+        if (idx == 1000) {
+            break;
+        }
     }
 
 
+
+
+
+
+
+
+
+    let mut correct : i64 = 0;
+    let mut total : i64 = 0;
+    idx = 0;
 
     for x in x_test_1 {
 
@@ -200,15 +243,31 @@ fn main() -> Result<(), Error> {
         a2 = l2.map(|x| nn::NN::sigmoid(x));
 
         lo = l2 * &w2o;
-        ao = lo.map(|x| nn::NN::sigmoid(x));
+        ao = lo.map(|x| nn::NN::softmax(x));
+
+        let mut max_idx = 0;
+        let mut max_val = 0.;
+        for i in 0..ao.len(){
+            print!("{} ", ao[(0,i)].clone());
+            if ao[(0,i)] > max_val {
+                max_val = ao[(0,i)].clone();
+                max_idx = i;
+            }
+        }
+        println!("");
         
         let y : i64 = y_test_1[idx].unwrap() as i64;
-        let mut ly = na::DMatrix::from_element(size_lo, 1, 0.);
-        ly[(y as usize,0)] = 1.;
+        if max_idx == y as usize {
+            correct += 1;
+        }
+
+        total += 1;
+
+        println!("{} - {}, {} / {} = {} %", y, max_idx, correct, total, (correct as f64 / total as f64) * 100.);
     }
 
 
-    
+
 
     Ok(())
 
